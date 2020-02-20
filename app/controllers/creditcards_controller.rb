1ff7cfbb03beeebffefcb4d338e2247974821987
nousi <nousidayo@gmail.com>
2020-02-21 04:35:37 +0900

    creditcards#newのリダイレクト処理を追加

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,10 +1,12 @@
 class CreditcardsController < ApplicationController
   require "payjp"
-  
+
   def index
   end
 
   def new
+    @card = Creditcard.where(user_id: current_user.id).first
+    redirect_to action: "index" if @card.present?    
   end
 
   def create
