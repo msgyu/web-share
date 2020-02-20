7f657fb36dda765c75e69f1707aaa727c1b790b0
nousi <nousidayo@gmail.com>
2020-02-21 04:41:42 +0900

    creditcards#createの処理を作成
    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]はPayjpの秘密鍵を環境変数で定義したもの。
    
    payjpはjsで処理を進めていくので、まずトークンがちゃんとあるのか？確認する。
    params['payjpToken']
    なければ、render "new"で戻る
    
    ちゃんとparams['payjpToken']があった場合、
    payjpのカスタマーを作成する。
    customer = Payjp::Customer.create(
            description: 'test',
            email: current_user.email,
            card: params['payjpToken'],
            metadata: {user_id: current_user.id}
          )
    
    Payjpに登録するためのカスタマーを作成できたら、クレジットカードを登録する
    @card = Creditcard.new(user_id: current_user.id, customer_id: customer.id)
            if @card.save
              if request.referer&.include?("/registrations/step5")
                redirect_to controller: 'registrations', action: "step6"
              else
                redirect_to action: "index", notice:"支払い情報の登録が完了しました"
              end
            else
              render 'new'
            end

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -10,6 +10,27 @@ class CreditcardsController < ApplicationController
   end
 
   def create
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    if params['payjpToken'].blank?
+      render "new"
+    else
+      customer = Payjp::Customer.create(
+        description: 'test',
+        email: current_user.email,
+        card: params['payjpToken'],
+        metadata: {user_id: current_user.id}
+      )
+      @card = Creditcard.new(user_id: current_user.id, customer_id: customer.id)
+        if @card.save
+          if request.referer&.include?("/registrations/step5")
+            redirect_to controller: 'registrations', action: "step6"
+          else
+            redirect_to action: "index", notice:"支払い情報の登録が完了しました"
+          end
+        else
+          render 'new'
+        end
+    end
   end
 
   def destroy
