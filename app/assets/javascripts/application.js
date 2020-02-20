e787c1349519c90dac95fe6692e62cf34230a375
nousi <nousidayo@gmail.com>
2020-02-20 20:29:36 +0900

    jscrollのイベントを作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,3 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-//= require jquery.jscroll.min.js
---
+++ app/assets/javascripts/pagination-jscroll.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('.jscroll').jscroll({
+    nextSelector: '.next'
+  });
+});
