99684150ae20a27d7e5ab6b400e60f5cbb3ea7a1
nousi <nousidayo@gmail.com>
2020-02-22 01:51:16 +0900

    検索結果をページネーション
    ページネーションとN+1問題の解消をしている。
    distinct: trueは重複レコードの検索を避けるため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
   def set_search
     @search = Product.ransack(params[:q]) #ransackメソッド推奨
-    @search_products = @search.result.page(params[:page])
+    @search_products = @search.result(distinct: true).order(created_at: "DESC").includes(:host).page(params[:page]).without_count.per(1)
   end
 end
