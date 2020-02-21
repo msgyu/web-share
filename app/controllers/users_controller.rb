4cd49670ab9a38bc05d39c0331993b1bd0fb1411
nousi <nousidayo@gmail.com>
2020-02-21 22:06:12 +0900

    searchアクションをapplication_controller.rbで定義
    users_controller.rbだとuser系以外で検索かけるとエラーが起きるため、application_controller.rbで定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  def search
-    @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
-  end
-
   def index
     @products = Product.order(created_at: "DESC").includes(:host).page(params[:page]).without_count.per(1)
   end
