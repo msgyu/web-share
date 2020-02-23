a7095d9a71930fcd49f13452827020f3774cf0f8
nousi <nousidayo@gmail.com>
2020-02-23 20:57:19 +0900

    親カテゴリのHTMLを定義
    JQueryでカテゴリを追加するが、おの親カテゴリである。

--- app/assets/javascripts/category-form.js
+++ app/assets/javascripts/category-form.js
@@ -1,6 +1,11 @@
 $(document).on('turbolinks:load', function(){
   var categoryBox = $('.form-details__form-box__category')
+  // 親カテゴリー
+  function appendOption(category) {
+    var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`
+  }
 
+  
   categoryBox.on("change", "#parent-category", function(){
     var parentCategory = $("parent-category").value;
     if(parentCategory !== "") {
