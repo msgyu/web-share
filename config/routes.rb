ff01fd33a3e6a7c22c4204c9199b09b9e8995395
nousi <nousidayo@gmail.com>
2020-02-23 19:33:52 +0900

    jsonを使ったroutingに設定

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,12 @@ Rails.application.routes.draw do
   end
   resources :creditcards, except: :show
   resources :products
+  collection do
+    get 'get_category_children', defaults: { format: 'json' }
+    get 'get_category_grandchildren', defaults: { format: 'json' }
+  end
+  member do
+    get 'get_category_children', defaults: { format: 'json' }
+    get 'get_category_grandchildren', defaults: { format: 'json' }
+  end
 end
