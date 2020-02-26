1f74e48797b7ed5dbf9395f5c1356a7d3d9a091c
nousi <nousidayo@gmail.com>
2020-02-26 20:09:55 +0900

    アソシエーションの再定義
    class_name: "product"ではエラーになるため、
    class_name: "Product"にした

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :products
-  has_many :bought_products, class_name: "product", foreign_key: "client_id"
-  has_many :selling_products, -> { where("client_id is NULL") }, class_name: "product", foreign_key: "host_id"
+  has_many :bought_products, class_name: "Product", foreign_key: "client_id"
+  has_many :selling_products, class_name: "Product", foreign_key: "host_id"
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
