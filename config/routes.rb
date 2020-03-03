8f4ad4e6bf5944003472659c5e2047252c54da1b
nousi <nousidayo@gmail.com>
2020-03-03 18:03:28 +0900

    プロフィール編集用のページとupdate処理を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get 'profile_edit', to: 'users/registrations#profile_edit', as: 'profile_edit'
+    put 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
+  end
+
   root 'users#index'
   resources :users do
     collection do
