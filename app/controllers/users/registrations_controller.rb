3dd65b585ea46e125084901b152ca7929978e230
nousi <nousidayo@gmail.com>
2020-03-03 18:04:12 +0900

    route.rbで作成した処理をコントローラーでも適用
    prodile_editとprofile_updateの処理を定義

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,6 +9,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  def profile_edit
+  end
+
+  def profile_update
+    current_user.assign_attributes(account_update_params)
+    if current_user.save
+	  redirect_to my_page_path, notice: 'プロフィールを更新しました'
+    else
+      render "profile_edit"
+    end
+  end
+
   # POST /resource
   # def create
   #   super
@@ -38,7 +50,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -46,9 +58,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
