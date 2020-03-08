816dbd99983c0ca9dc6461da74470b6cbc07fcf4
nousi <nousidayo@gmail.com>
2020-03-09 00:20:32 +0900

    製品を持たないUserを除外
    プランを定時しているUserのみを取得できるようにした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def index
     @products = Product.order(created_at: "DESC").includes(:host).page(params[:page]).without_count.per(5)
+    @hosts = User.joins('LEFT JOIN Products ON users.id = products.host_id').where('products.id IS NOT NULL').uniq
   end
 
   def search
