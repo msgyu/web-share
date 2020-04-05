ca84fc46c8f5683e3555ba8176bbe87ddc3ba2e6
nousi <nousidayo@gmail.com>
2020-04-05 18:15:38 +0900

    user側でreceiptのアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   has_many :sns
   has_many :bought_products, class_name: "Product", foreign_key: "client_id"
   has_one_attached :avatar
-
+  has_many :buy_receipts,  class_name: "Receipt", foreign_key: "buyer_id"
+  has_many :sold_receipts, class_name: "Receipt", foreign_key: "seller_id"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 end
