36d8b0d9e58ffa0d5c7b8521fb0fa5ad343543c3
nousi <nousidayo@gmail.com>
2020-07-07 23:29:39 +0900

    Rspecの範囲を設定
    一度テストしない範囲にはfalseを設定

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,11 @@ module WebShare
       g.javascripts false
       g.helper false
       g.test_framework false
-      
+      g.test_framework :rspec, 
+        view_specs: false, 
+        helper_specs: false, 
+        controller_specs: false, 
+        routing_specs: false
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
