fa3b671ad08ba3113a87454091b189c33f6e6852
nousi <nousidayo@gmail.com>
2020-07-07 22:36:04 +0900

    Fackerをランダムデータにするために、sequenceを追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,17 @@
 FactoryBot.define do
   factory :user, class: User do
-    nickname     { Faker::Name.name }
-    email        { Faker::Internet.email}
-    password                  {Faker::Internet.password(min_length: 8)}
-    password_confirmation     {Faker::Internet.password(min_length: 8)}
+    sequence(:nickname)                  { Faker::Name.name }
+    sequence(:email)                     { Faker::Internet.email}
+    sequence(:password)                  {Faker::Internet.password(min_length: 8)}
+    sequence(:password_confirmation)     {Faker::Internet.password(min_length: 8)}
     firstname                 {"田中"}
     lastname                  {"太郎"}
     firstname_kana            {"タナカ"}
     lastname_kana             {"タロウ"}
     birth_year                {"1999"}
-    birth_month               {Faker::Number.within(range: 1..12)}
-    birth_day                 {Faker::Number.within(range: 1..30)}
+    sequence(:birth_month)               {Faker::Number.within(range: 1..12)}
+    sequence(:birth_day)                 {Faker::Number.within(range: 1..30)}
+    sequence(:phone_number)   {Faker::PhoneNumber.phone_number}
   end
   
 end
