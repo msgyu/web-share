26b9f42911f960c0f8732852538c417e08110ea0
nousi <nousidayo@gmail.com>
2020-07-08 09:35:47 +0900

    userでvalidate条件を満たしているときのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
-    
+    # 1. ユーザーモデルのニックネーム、メールアドレス、氏名、氏名カナ、生年月日、電話番号があれば登録ができること
+    it "is valid with a nickname, email, firstname, lastname, firstname_kana, lastname_kana, birth_year, birth_month, birth_day, phone_number" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
   end
 end
