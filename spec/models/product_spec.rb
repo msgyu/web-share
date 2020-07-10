c67a7780d472de03c9b5726877218d75253cbe7e
nousi <nousidayo@gmail.com>
2020-07-10 19:24:25 +0900

    product test  (without :name )

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 describe Product do
   describe '#create' do
-    it "is valid with name, description, period, price"
+    it "is valid with name, description, period, price" do
       product = build(:product)
       product.valid?
       expect(product).to be_valid
     end
+    
+    it "is invalid without a name" do
+      product = build(:product)
+      product.valid?
+      expect(product.errors[:name]).to include("を入力してください")
+    end
+    
   end
 end
