148f307846c6a04c67d4b3ce09634f3cc1e4c3ad
nousi <nousidayo@gmail.com>
2020-07-10 19:22:39 +0900

    productのvalidateテスト（trueの場合)

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe Product do
   describe '#create' do
-    
+    it "is valid with name, description, period, price"
+      product = build(:product)
+      product.valid?
+      expect(product).to be_valid
+    end
   end
 end
