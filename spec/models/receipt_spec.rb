b96d538f5aafed1529a204065fb2b7ab5a57cc1b
nousi <nousidayo@gmail.com>
2020-07-12 21:48:30 +0900

    receipt test valide
    validationの条件を満たしている場合

--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe Receipt do
   describe '#create' do
-    
+    it "is valid with a name, price, buyer_id, seller_id, product_id" do
+      buyer = build(:user)
+      receipt = build(:receipt, buyer_id: buyer.id)
+      expect(user).to be_valid
+    end
   end
 end
