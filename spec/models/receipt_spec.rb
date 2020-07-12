c282b9f062ce2b68680cb33e918764802d3f66d2
nousi <nousidayo@gmail.com>
2020-07-12 21:50:33 +0900

    receipt test name:nil

--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -5,7 +5,12 @@ describe Receipt do
     it "is valid with a name, price, buyer_id, seller_id, product_id" do
       buyer = build(:user)
       receipt = build(:receipt, buyer_id: buyer.id)
-      expect(user).to be_valid
+      expect(receipt).to be_valid
+    end
+    it "is invalid without a name" do
+      buyer = build(:user)
+      receipt = build(:receipt, name: nil,  buyer_id: buyer.id)
+      expect(receipt).to include("を入力してください")
     end
   end
 end
