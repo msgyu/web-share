8008247f3f62e797d85877b7eaab3db7ed37becb
nousi <nousidayo@gmail.com>
2020-07-25 12:39:30 +0900

    receipt invalid without price

--- spec/models/receipt_spec.rb
+++ spec/models/receipt_spec.rb
@@ -10,7 +10,12 @@ describe Receipt do
     it "is invalid without a name" do
       buyer = build(:user)
       receipt = build(:receipt, name: nil,  buyer_id: buyer.id)
-      expect(receipt).to include("を入力してください")
+      expect(receipt.errors[:name]).to include("を入力してください")
+    end
+    it "is invalid without a price" do
+      buyer = build(:user)
+      receipt = build(:receipt, price: nil,  buyer_id: buyer.id)
+      expect(receipt.errors[:price]).to include("を入力してください")
     end
   end
 end
