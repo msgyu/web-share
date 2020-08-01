54df8a8034abeb756c0f7c8ebe85361eadab87ac
nousi <nousidayo@gmail.com>
2020-08-01 17:04:30 +0900

    delete "Faker::Internet.password(min_length: 8)"
    ハイフンがエラーの原因になるため、削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     sequence(:nickname)               { Faker::Name.name }
     sequence(:email)                  { Faker::Internet.email}
-    sequence(:password)               {Faker::Internet.password(min_length: 8)}
-    sequence(:password_confirmation)  {Faker::Internet.password(min_length: 8)}
+    sequence(:password)               {"12345678"}
+    sequence(:password_confirmation)  {"12345678"}
     firstname                         {"田中"}
     lastname                          {"太郎"}
     firstname_kana                    {"タナカ"}
@@ -11,7 +11,7 @@ FactoryBot.define do
     birth_year                        {"1999"}
     sequence(:birth_month)            {Faker::Number.within(range: 1..12)}
     sequence(:birth_day)              {Faker::Number.within(range: 1..30)}
-    sequence(:phone_number)           {Faker::PhoneNumber.phone_number}
+    sequence(:phone_number)           {"08012341234"}
   end
   
 end
