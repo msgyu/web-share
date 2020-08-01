6f5819fa5490ea9fb50d8ba91937a4049cf992f1
nousi <nousidayo@gmail.com>
2020-08-01 19:30:32 +0900

    add category = create(:category)

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,61 +1,71 @@
 require 'rails_helper'
 
-describe Product do
+RSpec.describe Product do
   describe '#create' do
     it "is valid with name, description, period, price" do
+      category = create(:category)
       product = build(:product)
       product.valid?
       expect(product).to be_valid
     end
     
     it "is invalid without a name" do
-      product.build(:product, name: nil)
+      user = create(:user)
+      product = user.products.build(:product, name: nil)
       product.valid?
       expect(product.errors[:name]).to include("を入力してください")
     end
     it "is invalid without a description" do
-      product.build(:product, description: nil)
+      user = create(:user)
+      product = user.products.build(:product, description: nil)
       product.valid?
       expect(product.errors[:description]).to include("を入力してください")
     end
 
     it "is invalid without a period" do
-      product.build(:product, period: nil)
+      user = create(:user)
+      product = user.products.build(:product, period: nil)
       product.valid?
       expect(product.errors[:period]).to include("を入力してください")
     end
     
     it "is invalid without a price" do
-      product.build(:product, price: nil)
+      user = create(:user)
+      product = user.products.build(:product, price: nil)
       product.valid?
       expect(product.errors[:price]).to include("を入力してください")
     end
 
     it "is valid with a name that has less than 40 characters" do
-      product.build(:product, name: "aaaaaaaaa!aaaaaaaaa!")
+      user = create(:user)
+      product = user.products.build(:product, name: "aaaaaaaaa!aaaaaaaaa!")
       product.valid?
       expect(product).to be_valid
     end
     it "is invalid with a name that has more than 40 characters" do
-      product.build(:product, name: "aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!")
+      user = create(:user)
+      product = user.products.build(:product, name: "aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!")
       product.valid?
       expect(product.errors[:name]).to include("は40文字以内に入力してください")
     end
 
     it "is valid with a description that has less than 40 characters" do
-      product.build(:product, description: "aaaaaaaaa!aaaaaaaaa!")
+      user = create(:user)
+      product = user.products.build(:product, description: "aaaaaaaaa!aaaaaaaaa!")
       product.valid?
       expect(product).to be_valid
     end
 
     it "is invalid with a description that has less than 300 characters" do
-      product.build(:product, description: "aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!")
+      user = create(:user)
+      product = user.products.build(:product, description: "aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!aaaaaaaaa!")
       product.valid?
       expect(product.errors[:description]).to include("は300文字以内に入力してください")
     end
 
     it "is invalid with price for text" do
-      product.build(:product, price: "aaaaa")
+      user = create(:user)
+      product = user.products.build(:product, price: "aaaaa")
       product.valid?
       expect(product.errors[:price]).to include("")
     end
