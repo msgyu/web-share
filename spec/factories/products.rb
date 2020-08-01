de80a59423a46760603d2308f2c5da38f5d09b99
nousi <nousidayo@gmail.com>
2020-08-01 19:28:46 +0900

    add category_ids

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :product do
     association :user
-    name        { "テスト名のプラン" }
-    description { "テスト用の記述です" }
-    period      {"7"}
-    price       {"5000"}
+    association :category
+    name         { "テスト名のプラン" }
+    description  { "テスト用の記述です" }
+    period       {"7"}
+    price        {"5000"}
+    user_id      {"1"}
+    category_ids {"1"}
   end
 end
