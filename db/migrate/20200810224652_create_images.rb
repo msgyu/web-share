03259e5f433bb58b796bbff18d7c3adff736ff81
nousi <nousidayo@gmail.com>
2020-08-11 07:57:12 +0900

    Add column

--- db/migrate/20200810224652_create_images.rb
+++ db/migrate/20200810224652_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-
+      t.string :name,                           null: false
+      t.references :product, foreign_key: true, null: false
       t.timestamps
     end
   end
