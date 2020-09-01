72f91aac8d352f12ce9dabe05cdc6a263da2a0f4
nousi <nousidayo@gmail.com>
2020-09-01 20:48:23 +0900

    requireを追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,7 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
