03e4b5371f40b57cd2bd7c231440f1774daad9b1
nousi <nousidayo@gmail.com>
2020-09-03 19:22:12 +0900

    コメントアウトを削除

--- app/assets/javascripts/product-image-form.js
+++ app/assets/javascripts/product-image-form.js
@@ -24,16 +24,10 @@ $(document).on("turbolinks:load", function () {
     }
   }
 
-  // プレビュー機能
-  //'change'イベントでは$(this)で要素が取得できないため、 'click'イベントを入れた。
-  //これにより$(this)で'input'を取得することができ、inputの親要素である'li'まで辿れる。
-
   $(document).on("click", ".image_upload", function () {
-    //$liに追加するためのプレビュー画面のHTML
     var preview = $(
-      '<div class="image-preview__wapper"><img class="preview"></div><div class="image-preview_btn"><div class="image-preview_btn_edit">編集</div><div class="image-preview_btn_delete">削除</div></div>'
+      `<div class="image-preview__wapper"><img class="preview"></div><div class="image-preview_btn"><div class="image-preview_btn_edit">編集</div><div class="image-preview_btn_delete">削除</div></div>`
     );
-    //次の画像を読み込むためのinput。処理の最後にappendで追加する。
     var append_input = $(
       `<li class="input"><label class="upload-label"><div class="upload-label__text">ドラッグアンドドロップ<br>またはクリックしてファイルをアップロード<div class="input-area"><input class="hidden image_upload" type="file"></div></div></label></li>`
     );
@@ -41,24 +35,15 @@ $(document).on("turbolinks:load", function () {
     $li = $(this).parents("li");
     $label = $(this).parents(".upload-label");
     $inputs = $ul.find(".image_upload");
-    //inputに画像を読み込んだら、"プレビューの追加"と"新しいli追加"処理が動く
+
     $(".image_upload").on("change", function (e) {
-      //inputで選択した画像を読み込む
       var reader = new FileReader();
-
-      // プレビューに追加させるために、inputから画像ファイルを読み込む。
       reader.readAsDataURL(e.target.files[0]);
-
-      //画像ファイルが読み込んだら、処理が実行される。
       reader.onload = function (e) {
-        //previewをappendで追加する前に、プレビューできるようにinputで選択した画像を<img>に'src'で付与させる
         $(preview).find(".preview").attr("src", e.target.result);
       };
 
-      //inputの画像を付与した,previewを$liに追加。
       $li.append(preview);
-
-      //プレビュー完了後は、inputを非表示にさせる。これによりプレビューだけが残る。
       $label.css("display", "none");
       $li.removeClass("input");
       $li.addClass("image-preview");
@@ -67,7 +52,7 @@ $(document).on("turbolinks:load", function () {
       $("#previews li").css({
         width: `114px`,
       });
-      //"ul"に新しい"li(inputボタン)"を追加させる。
+
       if ($lis.length <= 4) {
         $ul.append(append_input);
         $("#previews li:last-child").css({
@@ -87,9 +72,7 @@ $(document).on("turbolinks:load", function () {
         });
       }
 
-      //inputの最後の"data-image"を取得して、input nameの番号を更新させてる。
       $inputs.each(function (num, input) {
-        //nameの番号を更新するために、現在の番号を除去
         $(input).removeAttr("name");
         $(input).attr({
           name: "product[images_attributes][" + num + "][name]",
