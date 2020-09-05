f0bcf6817bc757a42da54740b0e6ffff0a1720ae
nousi <nousidayo@gmail.com>
2020-09-05 20:03:34 +0900

    Create index.html.haml

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,13 +4,14 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:aws_access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:aws_secret_access_key],
       region: 'ap-northeast-1'
     }
-
     config.fog_directory  = 'web-share-image'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/web-share-image'
     config.cache_storage = :fog
@@ -19,3 +20,5 @@ CarrierWave.configure do |config|
     config.enable_processing = false if Rails.env.test?
   end
 end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
